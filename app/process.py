cf4abfc2f064a2624d2f153352f0b0219e730f2e
G-coder <gregoire.woroniak@gmail.com>
2025-07-20 13:18:41 +0200

    remove log in ffmpeg and add img coordinates in process

--- app/process.py
+++ app/process.py
@@ -4,7 +4,7 @@ from app.core.partition import Partition
 from app.core.piano import Piano
 from app.utils import init_background
 from app.core.video import Video
-from app.core.sound import Instrument
+from app.sound import Instrument
 import subprocess
 
 def main(
@@ -15,15 +15,16 @@ def main(
     output_dir: Path,
     fps: int,
     sf2_path: Path,
+    timing: float,
 ):
-    background, img_bottom = init_background(img_path, height, width)
-    screen = Screen(background, img_bottom)
+    background, img_coordinates = init_background(img_path, height, width)
+    screen = Screen(background)
     partition = Partition(partition_path, fps=fps)
     piano = Piano(width=width, white_tile_height=55, sep_size=2)
     video = Video(screen, piano, fps=fps)
 
     video_path = output_dir / "video.mp4"
-    video.generate(partition, str(video_path))
+    video.generate(partition, str(video_path), img_coordinates, timing)
     print(f"\033[92mVideo released successfuly: {str(video_path)}\033[0m")
 
     audio_path = Path(output_dir / "audio.wav")
@@ -33,6 +34,7 @@ def main(
 
     cmd = [
         "ffmpeg", "-y",
+        "-loglevel", "error",
         "-i", str(video_path),    # Input 0
         "-i", str(audio_path),    # Input 1
         "-map", "0:v:0",          # Prendre la vidéo du premier input
